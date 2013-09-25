f82e0f74a8579747355a467dcf4e60781f51c056
Chun Mei <Chun Mei@localhost>
2013-09-25 21:13:12 +0800

    Feedback class toString() is now functioning.

--- CS2103T Project/src/Logic/Feedback.java
+++ CS2103T Project/src/Logic/Feedback.java
@@ -4,10 +4,7 @@ public class Feedback {
 	private CommandType feedbackCommand = null;
 	private String feedbackString = null;
 	private int statusCode = 0;
-
-	public Feedback(int status) {
-		statusCode = status;
-	}
+	private String statusMessage = null;
 
 	public Feedback(int status, CommandType command) {
 		statusCode = status;
@@ -37,14 +34,144 @@ public class Feedback {
 	}
 
 	public String toString() {
-		switch (statusCode) {
-		case 10:
-			return "";
-		case 61:
-			return Constants.MSG_LINE_NUMBER_OVERFLOW;
-		default:
-			return "";
+		switch (feedbackCommand) {
+		case ADD_TASK :
+			return addFeedback();
+		case EDIT_TASK:
+			return editFeedback();
+		case DISPLAY :
+			return displayFeedback();
+		case DELETE :
+			return deleteFeedback();
+		case CLEAR :
+			return clearFeedback();
+		case SORT :
+			return sortFeedback();
+		case SEARCH:
+			return searchFeedback();
+		case INVALID :
+			return invalidFeedback();
+		case UNDO :
+			return undoFeedback();
+		case FINALISE :
+			return finaliseFeedback();
+		case HELP :
+			return helpFeedback();
+		case DONE :
+			return doneFeedback();
+		case EXIT :
+			System.exit(0);
+		default :
+			throw new Error(Constants.MSG_UNRECOGNISED_COMMAND);
 		}
+	}
 
+	private String addFeedback() {
+		if (statusCode == Constants.SC_SUCCESS) {
+			statusMessage = "Task added successfully! \n" + feedbackString;
+		} else if (statusCode == Constants.SC_SUCCESS_TASK_OVERDUE) {
+			statusMessage = "Task added successfully! Task is overdue \n" + feedbackString;
+		} else if (statusCode == Constants.SC_EMPTY_DESCRIPTION_ERROR) {
+			statusMessage = "Error: No task description was given";
+		} else if (statusCode == Constants.SC_UNRECOGNISED_ATTRIBUTE_ERROR) {
+			statusMessage = "Error: Attribute is unrecognised";
+		} else {
+			statusMessage = "Error: Invalid add (this should not happen!)";
+		}
+		return statusMessage;
+	}
+
+	private String editFeedback() {
+		if (statusCode == Constants.SC_SUCCESS) {
+			statusMessage = "Task edited successfully! \n" + feedbackString;
+		} else if (statusCode == Constants.SC_SUCCESS_TASK_OVERDUE) {
+			statusMessage = "Task edited successfully! Task is overdue \n" + feedbackString;
+		} else if (statusCode == Constants.SC_EMPTY_DESCRIPTION_ERROR) {
+			statusMessage = "Error: No task description was given";
+		} else if (statusCode == Constants.SC_UNRECOGNISED_ATTRIBUTE_ERROR) {
+			statusMessage = "Error: Attribute is unrecognised";
+		} else if (statusCode == Constants.SC_INTEGER_OUT_OF_BOUNDS_ERROR) {
+			statusMessage = "Error: ID is not valid";
+		} else if (statusCode == Constants.SC_NO_ID_INDICATED_ERROR) {
+			statusMessage = "Error: No ID is indicated";
+		} else {
+			statusMessage = "Error: Invalid edit (this should not happen!)";
+		}
+		return statusMessage;
+	}
+
+	private String displayFeedback() {
+		if (statusCode == Constants.SC_SUCCESS) {
+			statusMessage = feedbackString;
+		} else if (statusCode == Constants.SC_DISPLAY_NO_TASK_ERROR) {
+			statusMessage = "You have no tasks :)";
+		} else {
+			statusMessage = "Error: Invalid display (this should not happen!)";
+		}
+		return statusMessage;
+	}
+
+	private String deleteFeedback() {
+		if (statusCode == Constants.SC_SUCCESS) {
+			statusMessage = "Task deleted successfully! \n" + feedbackString;
+		} else if (statusCode == Constants.SC_INTEGER_OUT_OF_BOUNDS_ERROR) {
+			statusMessage = "Error: ID is not valid";
+		} else if (statusCode == Constants.SC_NO_ID_INDICATED_ERROR) {
+			statusMessage = "Error: No ID is indicated";
+		} else {
+			statusMessage = "Error: Invalid delete (this should not happen!)";
+		}
+		return statusMessage;
+	}
+
+	private String clearFeedback() {
+		if (statusCode == Constants.SC_SUCCESS) {
+			statusMessage = "All tasks cleared!";
+		} else if (statusCode == Constants.SC_NO_TASK_TO_CLEAR_ERROR) {
+			statusMessage = "Error: You have no tasks to clear!";
+		} else {
+			statusMessage = "Error: Invalid clear (this should not happen!)";
+		}
+		return statusMessage;
+	}
+
+	private String sortFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String searchFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String invalidFeedback() {
+		if (statusCode == Constants.SC_INVALID_COMMAND_ERROR) {
+			statusMessage = "Error: Invalid command!";
+		} else {
+			// should not happen
+			statusMessage = "Error: Invalid command (this should not happen!)";
+		}
+		return statusMessage;
+	}
+
+	private String undoFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String finaliseFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String helpFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String doneFeedback() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
