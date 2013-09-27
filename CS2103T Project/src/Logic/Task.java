41b786dc14ce1e330802904ab7aa87abf4e95e52
khorshijie <khorshijie@localhost>
2013-09-28 01:26:16 +0800

    Modified logic and task to fit the current command interface

--- CS2103T Project/src/Logic/Task.java
+++ CS2103T Project/src/Logic/Task.java
@@ -9,6 +9,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.joda.time.DateTime;
+
 public class Task {
 	public static void main(String[] args) {
 		HashMap<String, String> testMap1 = new HashMap<String, String>();
@@ -35,50 +37,29 @@ public class Task {
 	private String type = "";
 	private String location = "";
 	private List<String> tags = new ArrayList<String>();
-	private Date startTime = null;
-	private Date endTime = null;
-	private Date deadline = null;
-	private List<Slot> possibleTime = new ArrayList<Slot>();
+	private Interval interval = null;
+	private DateTime deadline = null;
+	private List<Interval> possibleIntervals = new ArrayList<Interval>();
 	private boolean done = false;
 
 	private SimpleDateFormat dateParser = new SimpleDateFormat("h:mm a");
 
-	public Task(HashMap<String, String> attributes) {
-		try {
-			name = attributes.get(Constants.TASK_ATT_NAME);
-			type = attributes.get(Constants.TASK_ATT_TYPE);
-			if (attributes.containsKey(Constants.TASK_ATT_LOCATION)) {
-				location = attributes.get(Constants.TASK_ATT_LOCATION);
-			}
-			
-			done = false;
-			if (attributes.containsKey(Constants.TASK_ATT_TAGS)) {
-				tags = tagsParser(attributes.get(Constants.TASK_ATT_TAGS));
-			}
-			if (attributes.containsKey(Constants.TASK_ATT_STARTTIME)) {
-				startTime = dateParser.parse(attributes
-						.get(Constants.TASK_ATT_STARTTIME));
-			}
-			if (attributes.containsKey(Constants.TASK_ATT_ENDTIME)) {
-				endTime = dateParser.parse(attributes
-						.get(Constants.TASK_ATT_ENDTIME));
-			}
-			if (attributes.containsKey(Constants.TASK_ATT_DEADLINE)) {
-				deadline = dateParser.parse(attributes
-						.get(Constants.TASK_ATT_DEADLINE));
-			}
-			if (attributes.containsKey(Constants.TASK_ATT_POSSIBLETIME)) {
-				possibleTime = possibleTimeParser(attributes
-						.get(Constants.TASK_ATT_POSSIBLETIME));
-			}
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	public Task(Command command) {
+		name = command.getDescription();
+		type = command.getTaskType();
+		done = false;
 
-	private List<String> tagsParser(String tags) {
-		return Arrays.asList(tags.split(" "));
+		if (type == Constants.TASK_TYPE_TIMED) {
+			interval = command.getIntervals().get(0);
+		} else if (type == Constants.TASK_TYPE_DEADLINE) {
+			deadline = command.getDeadline();
+		} else if (type == Constants.TASK_TYPE_FLOATING) {
+			possibleIntervals = command.getIntervals(); 
+		}
+
+		if (command.getTags() != null) {
+			tags = command.getTags();
+		}
 	}
 
 	private String tagsToString() {
@@ -87,53 +68,6 @@ public class Task {
 			output.append("#" + tag + " ");
 		}
 		return output.toString();
-	}
-
-	private List<Slot> possibleTimeParser(String possibleTime) {
-		List<Slot> output = new ArrayList<Slot>();
-		String[] listOfTime = possibleTime.split(";");
-
-		for (int i = 0; i < listOfTime.length; i += 2) {
-			try {
-				Date startTime = dateParser.parse(listOfTime[i]);
-				Date endTime = dateParser.parse(listOfTime[i + 1]);
-				output.add(new Slot(startTime, endTime));
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		return output;
-	}
-
-	public void set(String attribute, String value) {
-		try {
-			if (attribute == Constants.TASK_ATT_DEADLINE) {
-				deadline = dateParser.parse(value);
-			} else if (attribute == Constants.TASK_ATT_DONE) {
-				done = Boolean.parseBoolean(value);
-			} else if (attribute == Constants.TASK_ATT_LOCATION) {
-				location = value;
-			} else if (attribute == Constants.TASK_ATT_NAME) {
-				name = value;
-			} else if (attribute == Constants.TASK_ATT_TYPE) {
-				type = value;
-			} else if (attribute == Constants.TASK_ATT_STARTTIME) {
-				startTime = dateParser.parse(value);
-			} else if (attribute == Constants.TASK_ATT_POSSIBLETIME) {
-				possibleTime = possibleTimeParser(value);
-			} else if (attribute == Constants.TASK_ATT_ENDTIME) {
-				endTime = dateParser.parse(value);
-			} else if (attribute == Constants.TASK_ATT_TAGS) {
-				tags = tagsParser(value);
-			} else {
-				// Nothing happens here!
-			}
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	public String get(String attribute) {
@@ -150,11 +84,11 @@ public class Task {
 		} else if (attribute == Constants.TASK_ATT_TYPE) {
 			output = type;
 		} else if (attribute == Constants.TASK_ATT_STARTTIME) {
-			output = startTime.toLocaleString();
+			output = interval.getStart().toString();
 		} else if (attribute == Constants.TASK_ATT_POSSIBLETIME) {
-			output = possibleTime.toString();
+			output = possibleIntervals.toString();
 		} else if (attribute == Constants.TASK_ATT_ENDTIME) {
-			output = endTime.toLocaleString();
+			output = interval.getEnd().toString();
 		} else if (attribute == Constants.TASK_ATT_TAGS) {
 			output = tagsToString();
 		} else {
@@ -212,36 +146,36 @@ public class Task {
 		this.tags = tags;
 	}
 
-	public Date getStartTime() {
-		return startTime;
+	public DateTime getStartTime() {
+		return interval.getStart();
 	}
 
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
+	public void setStartTime(DateTime startTime) {
+		interval.setStart(startTime);
 	}
 
-	public Date getEndTime() {
-		return endTime;
+	public DateTime getEndTime() {
+		return interval.getEnd();
 	}
 
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
+	public void setEndTime(DateTime endTime) {
+		interval.setEnd(endTime);
 	}
 
-	public Date getDeadline() {
+	public DateTime getDeadline() {
 		return deadline;
 	}
 
-	public void setDeadline(Date deadline) {
+	public void setDeadline(DateTime deadline) {
 		this.deadline = deadline;
 	}
 
-	public List<Slot> getPossibleTime() {
-		return possibleTime;
+	public List<Interval> getPossibleTime() {
+		return possibleIntervals;
 	}
 
-	public void setPossibleTime(List<Slot> possibleTime) {
-		this.possibleTime = possibleTime;
+	public void setPossibleTime(List<Interval> possibleIntervals) {
+		this.possibleIntervals = possibleIntervals;
 	}
 
 	public boolean isDone() {
@@ -283,19 +217,19 @@ public class Task {
 		if (isDeadlineTask()) {
 			output.append(" before " + deadline.toString());
 		} else if (isTimedTask()) {
-			output.append(" from " + startTime.toString() + " to "
-					+ endTime.toString());
+			output.append(" from " + getStartTime().toString() + " to "
+					+ getEndTime().toString());
 		} else if (isFloatingTask()) {
 			output.append(" on ");
 			int index = 1;
-			for (Slot slot : possibleTime) {
+			for (Interval slot : possibleIntervals) {
 				output.append("(");
 				output.append(index);
 				output.append(") ");
-				output.append(slot.getStartTime().toLocaleString());
+				output.append(slot.getStart().toString());
 				output.append(" to ");
-				output.append(slot.getEndTime().toLocaleString());
-				if (index != possibleTime.size()) {
+				output.append(slot.getEnd().toString());
+				if (index != possibleIntervals.size()) {
 					output.append(" or ");
 				}
 				index++;
