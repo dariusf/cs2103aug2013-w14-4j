4732cec0fd8767f8aac3a81c78e53c555cac4837
Chun Mei <Chun Mei@localhost>
2013-10-02 13:58:26 +0800

    Divided windows

--- CS2103T Project/src/UI/ApplicationWindow.java
+++ CS2103T Project/src/UI/ApplicationWindow.java
@@ -19,6 +19,7 @@ public class ApplicationWindow {
 	private Text input;
 	private Text displayFeedback;
 	private static Logic logic;
+	private Text displayTask;
 	
 	/**
 	 * Launch the application.
@@ -26,8 +27,8 @@ public class ApplicationWindow {
 	 */
 	public static void main(String[] args) {
 		try {
-			ApplicationWindow window = new ApplicationWindow();
 			logic = new Logic();
+			ApplicationWindow window = new ApplicationWindow();
 			window.open();	
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -56,19 +57,29 @@ public class ApplicationWindow {
 		shell = new Shell();
 		shell.setSize(512, 300);
 		shell.setText(Constants.APP_NAME);
-		shell.setLayout(new GridLayout(1, false));
+		shell.setLayout(new GridLayout(2, false));
 		
 		displayFeedback = new Text(shell, SWT.BORDER | SWT.READ_ONLY | SWT.MULTI);
 		displayFeedback.setEnabled(false);
 		GridData gd_feedback = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gd_feedback.widthHint = 302;
 		gd_feedback.heightHint = 215;
 		displayFeedback.setLayoutData(gd_feedback);
+		
+		displayTask = new Text(shell, SWT.BORDER | SWT.READ_ONLY | SWT.MULTI);
+		displayTask.setEnabled(false);
+		GridData gd_displayTask = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 2);
+		gd_displayTask.widthHint = 153;
+		gd_displayTask.heightHint = 230;
+		displayTask.setLayoutData(gd_displayTask);
 		
 		input = new Text(shell, SWT.BORDER);
 		GridData gd_input = new GridData(SWT.FILL, SWT.FILL, true, false);
 		gd_input.heightHint = 16;
 		input.setLayoutData(gd_input);
 
+		displayFeedback.setText(displayWelcomeMessage());
+		
 		enterDriverLoop();
 	}
 
@@ -81,6 +92,7 @@ public class ApplicationWindow {
 		input.addKeyListener(new KeyListener() {
 			String userInput = "";
 			String output = displayWelcomeMessage();
+			String tasks = "";
 			
 			@Override
 			public void keyReleased(KeyEvent arg0) {}
@@ -95,6 +107,9 @@ public class ApplicationWindow {
 					output = output + "\n" + feedback;
 					displayFeedback.setText(output);
 					input.setText("");
+					
+					tasks = logic.displayOnWindow();
+					displayTask.setText(tasks);
 				}
 			}
 			
