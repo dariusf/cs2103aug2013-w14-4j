e8c50f833e7a6d704c885de7a018e5e8fcc1efd0
Chun Mei <Chun Mei@localhost>
2013-10-23 13:49:27 +0800

    Commit windows classpath

---
+++ src/logic/ActiveFeedback.java
@@ -0,0 +1,13 @@
+package logic;
+
+import org.joda.time.DateTime;
+
+import common.CommandType;
+import common.DisplayMode;
+
+public class ActiveFeedback {
+	private CommandType feedbackCommand = null;
+	private int taskIndex = 0; 
+	private DisplayMode displayMode; 
+	
+}
