ab4b0d8c335106dc8948781904325ca9a3754e6d
Darius Foo <darius.foo.tw@gmail.com>
2013-10-24 15:03:33 +0800

    Getters and setters for TaskComposite

--- src/ui/TaskComposite.java
+++ src/ui/TaskComposite.java
@@ -16,57 +16,103 @@ import com.sun.accessibility.internal.resources.accessibility;
 
 public class TaskComposite extends Composite {
 
-    private static RowLayout innerRowLayout = new RowLayout();
-    
-    public TaskComposite(Composite parent, Task task, int index) {
-        super(parent, SWT.NONE);
+	private static final RowLayout innerRowLayout = new RowLayout();
 
-        // 340 is the fixed width and 69 is the fixed height. use SWT.default if
-        // you do not want to fix the lengths.
-        this.setLayoutData(new RowData(415, SWT.DEFAULT));
-        this.setLayout(innerRowLayout);
+	private StyledText taskIndex;
+	private StyledText taskName;
+	private StyledText taskDescription;
 
-        RowData taskIndexLayoutData = new RowData(60, 73);
-        RowData paddingLayoutData = new RowData(8, SWT.DEFAULT);
-        RowData taskDescriptionLayoutData = new RowData(330, SWT.DEFAULT);
+	public TaskComposite(Composite parent, Task task, int index) {
+		super(parent, SWT.NONE);
 
-        StyledText taskIndex = new StyledText(this, SWT.WRAP);
-        taskIndex.setText(String.valueOf(index));
-        taskIndex.setFont(ApplicationWindow.self.indexFont);
-        taskIndex.setForeground(new Color(ApplicationWindow.self.shell.getDisplay(), 0x99, 0, 0));
-        taskIndex.setLineAlignment(0, 1, SWT.RIGHT);
-        taskIndex.setLayoutData(taskIndexLayoutData);
+		// 340 is the fixed width and 69 is the fixed height. use SWT.default if
+		// you do not want to fix the lengths.
+		this.setLayoutData(new RowData(415, SWT.DEFAULT));
+		this.setLayout(innerRowLayout);
 
-        Composite paddingComposite = new Composite(this, SWT.NONE);
-        paddingComposite.setLayoutData(paddingLayoutData);
+		RowData taskIndexLayoutData = new RowData(60, 73);
+		RowData paddingLayoutData = new RowData(8, SWT.DEFAULT);
+		RowData taskDescriptionLayoutData = new RowData(330, SWT.DEFAULT);
 
-        Composite taskDetailsComposite = new Composite(this,
-                SWT.NONE);
-        taskDetailsComposite.setLayoutData(taskDescriptionLayoutData);
-        taskDetailsComposite.setLayout(innerRowLayout);
+		taskIndex = new StyledText(this, SWT.WRAP | SWT.READ_ONLY);
+		taskIndex.setText(String.valueOf(index));
+		taskIndex.setFont(ApplicationWindow.self.indexFont);
+		taskIndex.setForeground(new Color(ApplicationWindow.self.shell.getDisplay(), 0x99, 0, 0));
+		taskIndex.setLineAlignment(0, 1, SWT.RIGHT);
+		taskIndex.setLayoutData(taskIndexLayoutData);
 
-        StyledText taskName = new StyledText(taskDetailsComposite, SWT.READ_ONLY);
-        taskName.setText(task.getName());
-        taskName.setFont(ApplicationWindow.self.titleFont);
-        if (task.isDone()) {
-            StyleRange style1 = new StyleRange();
-            style1.start = 0;
-            style1.length = task.getName().length();
-            style1.strikeout = true;
-            taskName.setStyleRange(style1);
-        } else if (task.isOverdue()) {
-            taskName.setForeground(new Color(ApplicationWindow.self.shell.getDisplay(), 0x99, 0, 0));
-        }
-        taskName.setLayoutData(taskDescriptionLayoutData);
+		Composite paddingComposite = new Composite(this, SWT.NONE);
+		paddingComposite.setLayoutData(paddingLayoutData);
 
-        StyledText taskDescription = new StyledText(taskDetailsComposite,
-                SWT.READ_ONLY);
-        taskDescription.setText(task.getInfoString());
-        taskDescription.setFont(ApplicationWindow.self.descriptionFont);
+		Composite taskDetailsComposite = new Composite(this,
+				SWT.NONE);
+		taskDetailsComposite.setLayoutData(taskDescriptionLayoutData);
+		taskDetailsComposite.setLayout(innerRowLayout);
 
-        taskDetailsComposite.pack();
+		taskName = new StyledText(taskDetailsComposite, SWT.READ_ONLY);
+		taskName.setText(task.getName());
+		taskName.setFont(ApplicationWindow.self.titleFont);
 
-        this.pack();
-    }
+		taskName.setLayoutData(taskDescriptionLayoutData);
 
+		taskDescription = new StyledText(taskDetailsComposite,
+				SWT.READ_ONLY);
+		taskDescription.setText(task.getInfoString());
+		taskDescription.setFont(ApplicationWindow.self.descriptionFont);
+
+		if (task.isDone()) {
+			StyleRange style1 = new StyleRange();
+			style1.start = 0;
+			style1.length = task.getName().length();
+			style1.strikeout = true;
+			taskName.setStyleRange(style1);
+		} else if (task.isOverdue()) {
+			taskName.setForeground(new Color(ApplicationWindow.self.shell.getDisplay(), 0x99, 0, 0));
+		}
+
+		taskDetailsComposite.pack();
+
+		this.pack();
+	}
+
+	public void setTaskIndex(int index) {
+		taskIndex.setText(String.valueOf(index));
+	}
+
+	public int getTaskIndex() {
+		int result = -1;
+		try {
+			result = Integer.parseInt(taskIndex.getText());
+		}
+		catch (NumberFormatException e) {
+			assert false : "Invaild task index text stored";
+		}
+		assert result >= 0;
+		return result;
+	}
+
+	public void setTaskName(String name) {
+		taskName.setText(name);
+	}
+
+	public String getTaskName(String name) {
+		return taskName.getText();
+	}
+
+	public void setDescriptionName(String name) {
+		taskDescription.setText(name);
+	}
+
+	public String getDescriptionName(String name) {
+		return taskDescription.getText();
+	}
+	
+	public void setHighlighted(boolean highlighted) {
+		if (highlighted) {
+			setBackground(new Color(ApplicationWindow.self.shell.getDisplay(), 0xdd, 0xdd, 0xdd));
+		}
+		else {
+			setBackground(null);
+		}
+	}
 }
