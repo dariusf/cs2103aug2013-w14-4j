50fc6124303b0303a36954fe9ec042993e6edd76
khorshijie <khorshijie@localhost>
2013-10-26 01:17:14 +0800

    Fixed storage cloner bug and Date mode bug

---
+++ src/logic/Command.java
@@ -0,0 +1,247 @@
+package logic;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.joda.time.DateTime;
+
+import common.CommandType;
+import common.Constants;
+import common.DisplayMode;
+import common.InvalidCommandReason;
+
+
+public class Command {
+	private CommandType commandType = null;
+	
+	private String description = "";
+	private DateTime deadline = null;
+	private ArrayList<Interval> intervals = new ArrayList<>();
+	private ArrayList<String> tags = new ArrayList<>();
+
+	private int taskIndex = -1;
+	private int pageIndex = -1;
+	private int timeslotIndex = -1;
+	
+	private boolean clearDone = false;
+	private CommandType helpCommand;
+	private String searchString = "";
+	private InvalidCommandReason invalidCommandReason;
+	private DisplayMode displayMode;
+	private DateTime displayDateTime = null;
+	
+	
+	// TODO: remove
+	@Deprecated
+	private HashMap<String, String> commandAttributes = null;
+	
+	public Command(CommandType type){
+		commandAttributes = new HashMap<String, String>();
+		commandType = type;
+	}
+
+	public CommandType getCommandType(){
+		return commandType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String text) {
+		this.description = text;
+	}
+
+	public DateTime getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(DateTime deadline) {
+		this.deadline = deadline;
+	}
+
+	public ArrayList<Interval> getIntervals() {
+		return intervals;
+	}
+
+	public void setIntervals(ArrayList<Interval> intervals) {
+		this.intervals = intervals;
+	}
+	
+	public ArrayList<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+	
+	public int getTaskIndex() {
+		return taskIndex;
+	}
+
+	public void setTaskIndex(int taskIndex) {
+		this.taskIndex = taskIndex;
+	}
+
+	public int getTimeslotIndex() {
+		return timeslotIndex;
+	}
+
+	public void setTimeslotIndex(int timeslotIndex) {
+		this.timeslotIndex = timeslotIndex;
+	}
+
+	public DateTime getDisplayDateTime() {
+		return displayDateTime;
+	}
+
+	public void setDisplayDateTime(DateTime searchDateTime) {
+		this.displayDateTime = searchDateTime;
+	}
+	
+	public boolean getClearDone() {
+		return clearDone;
+	}
+
+	public void setClearDone(boolean clearDone) {
+		this.clearDone = clearDone;
+	}
+	
+	public CommandType getHelpCommand() {
+		return helpCommand;
+	}
+
+	public void setHelpCommand(CommandType helpCommand) {
+		this.helpCommand = helpCommand;
+	}
+
+	public String getSearchString() {
+		return searchString;
+	}
+
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+
+	public InvalidCommandReason getInvalidCommandReason() {
+		return invalidCommandReason;
+	}
+
+	public void setInvalidCommandReason(InvalidCommandReason invalidCommandReason) {
+		this.invalidCommandReason = invalidCommandReason;
+	}
+
+	// TODO: remove
+	@Deprecated
+	public HashMap<String, String> getCommandAttributes(){
+		return commandAttributes;
+	}
+	
+	// TODO: remove
+	@Deprecated
+	public void setValue(String attribute, String value){
+		commandAttributes.put(attribute, value);
+	}
+	
+	// TODO: might want to move to a higher level,
+	// this is just here for now to illustrate how these
+	// fields alone can define the task type clearly
+	public String getTaskType() {
+		assert commandType == CommandType.ADD || commandType == CommandType.EDIT;
+		if (deadline != null) {
+			return Constants.TASK_TYPE_DEADLINE;
+		}
+		else if (intervals.size() == 0) {
+			return Constants.TASK_TYPE_UNTIMED;
+		}
+		else if (intervals.size() == 1) {
+			return Constants.TASK_TYPE_TIMED;
+		}
+		else {
+			return Constants.TASK_TYPE_FLOATING;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Command [commandType=" + commandType + ", description="
+				+ description + ", deadline=" + deadline + ", intervals="
+				+ intervals + ", tags=" + tags + ", taskIndex=" + taskIndex
+				+ ", finaliseIndex=" + timeslotIndex + ", displayDateTime="
+				+ displayDateTime + ", clearDone=" + clearDone
+				+ ", helpCommand=" + helpCommand + ", searchString="
+				+ searchString + ", displayMode="
+						+ displayMode+ ", invalidCommandReason="
+				+ invalidCommandReason + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Command other = (Command) obj;
+		if (clearDone != other.clearDone)
+			return false;
+		if (commandType != other.commandType)
+			return false;
+		if (deadline == null) {
+			if (other.deadline != null)
+				return false;
+		} else if (!deadline.equals(other.deadline))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (displayDateTime == null) {
+			if (other.displayDateTime != null)
+				return false;
+		} else if (!displayDateTime.equals(other.displayDateTime))
+			return false;
+		if (timeslotIndex != other.timeslotIndex)
+			return false;
+		if (helpCommand != other.helpCommand)
+			return false;
+		if (intervals == null) {
+			if (other.intervals != null)
+				return false;
+		} else if (!intervals.equals(other.intervals))
+			return false;
+		if (invalidCommandReason != other.invalidCommandReason)
+			return false;
+		if (searchString == null) {
+			if (other.searchString != null)
+				return false;
+		} else if (!searchString.equals(other.searchString))
+			return false;
+		if (tags == null) {
+			if (other.tags != null)
+				return false;
+		} else if (!tags.equals(other.tags))
+			return false;
+		if (taskIndex != other.taskIndex)
+			return false;
+		return true;
+	}
+
+	public DisplayMode getDisplayMode() {
+		return displayMode;
+	}
+
+	public void setDisplayMode(DisplayMode displayMode) {
+		this.displayMode = displayMode;
+	}
+
+	public int getPageIndex() {
+		return pageIndex;
+	}
+
+	public void setPageIndex(int pageIndex) {
+		this.pageIndex = pageIndex;
+	}
+}
---
+++ src/ui/DisplayLogic.java
@@ -0,0 +1,304 @@
+package ui;
+
+import java.io.Console;
+import java.util.ArrayList;
+
+import org.eclipse.swt.widgets.Composite;
+import org.joda.time.DateTime;
+
+import com.joestelmach.natty.generated.DateParser_NumericRules.int_00_to_23_optional_prefix_return;
+
+import common.Constants;
+import common.DisplayMode;
+
+import logic.Feedback;
+import logic.Logic;
+import logic.Task;
+
+public class DisplayLogic {
+
+	private Logic logic;
+	private DisplayMode displayMode;
+	private Composite displayTask;
+	private int pageNumber;
+	private DisplayStateHistory displayStateHistory;
+
+	private org.joda.time.DateTime currentDisplayDateTime = new org.joda.time.DateTime();
+
+	private int noOfTasksToday = 0;
+	private int noOfTasksRemaining = 0;
+	private ArrayList<Integer> numberOfTasksOnEachPage;
+
+	private int taskCompositeHeight = 0;
+	private int taskCompositeIncrement = 0;
+	private int taskCompositeHeightForThreeLines = 0;
+
+	private Composite[] taskComposites = null;
+
+	public DisplayLogic(Logic logic, DisplayMode displayMode,
+			Composite displayTask, int pageNumber) {
+		setLogic(logic);
+		setDisplayMode(displayMode);
+		setDisplayTask(displayTask);
+		setPageNumber(pageNumber);
+		this.displayStateHistory = new DisplayStateHistory();
+	}
+
+	protected void setTaskCompositeHeight(int height) {
+		assert (taskCompositeHeight >= 0);
+		taskCompositeHeight = height;
+	}
+
+	protected void setTaskCompositeIncrement(int increment) {
+		assert (taskCompositeIncrement >= 0);
+		taskCompositeIncrement = increment;
+	}
+
+	protected void setTaskCompositeHeightForThreeLines(int height) {
+		assert (taskCompositeHeightForThreeLines >= 0);
+		taskCompositeHeightForThreeLines = height;
+	}
+	
+	private void setLogic(Logic logic) {
+		assert (logic != null);
+		this.logic = logic;
+	}
+
+	protected void setDisplayMode(DisplayMode displayMode) {
+		assert (displayMode != null);
+		this.displayMode = displayMode;
+	}
+	
+	private void setDisplayTask(Composite displayTask) {
+		assert (displayTask != null);
+		this.displayTask = displayTask;
+	}
+
+	protected void setDisplayDateTime(
+			org.joda.time.DateTime currentDisplayDateTime) {
+		assert (currentDisplayDateTime != null);
+		this.currentDisplayDateTime = currentDisplayDateTime;
+	}
+
+	protected void setPageNumber(int pageNumber) {
+		assert (pageNumber > 0);
+		this.pageNumber = pageNumber;
+	}
+
+	protected int getPageNumber() {
+		assert (pageNumber > 0);
+		return pageNumber;
+	}
+
+	protected DisplayMode getDisplayMode() {
+		return displayMode;
+	}
+
+	protected int getNumberOfRemainingTasks() {
+		noOfTasksRemaining = logic.getNumberOfRemainingTasks();
+		assert (noOfTasksRemaining >= 0);
+		return noOfTasksRemaining;
+	}
+
+	protected int getNumberOfTasksToday() {
+		noOfTasksToday = logic.getNumberOfTasksToday();
+		assert (noOfTasksToday >= 0);
+		return noOfTasksToday;
+	}
+
+	public String getDisplayWindowTitle() {
+		return determineTitle();
+	}
+
+	protected ArrayList<Integer> getNumberOfTasksForEachPage() {
+		determineNumberOfTasksForEachPage(displayMode);
+		return numberOfTasksOnEachPage;
+	}
+
+	public int getPage(int index) {
+		int page = 1;
+		int count = 0;
+		for (int i = 0; i < numberOfTasksOnEachPage.size(); i++) {
+			count += numberOfTasksOnEachPage.get(i);
+			if (index <= count) {
+				return page;
+			} else {
+				page++;
+			}
+		}
+		return page;
+	}
+	
+	public Composite getCompositeByIndex (int index) {
+		assert getPageNumber() == getPage(index) : "Cannot get composite of task that isn't currently displayed";
+		int tasksOnThisPage = numberOfTasksOnEachPage.get(getPageNumber());
+		// get index of first task on this page
+		int indexOfFirstTask = 1;
+		for (int i=0; i<getPageNumber()-1; i++) {
+			indexOfFirstTask += numberOfTasksOnEachPage.get(i);
+		}
+		return taskComposites[index - indexOfFirstTask];
+	}
+
+	private void determineNumberOfTasksForEachPage(DisplayMode displayMode) {
+		ArrayList<Task> taskList = logic.getTasksToDisplay(displayMode, currentDisplayDateTime);
+		int numberOfTasks = taskList.size();
+		int[] heights = new int[numberOfTasks];
+		int index = 0;
+		for (Task task : taskList) {
+			heights[index] = determineTaskHeight(task);
+			index++;
+		}
+
+
+		numberOfTasksOnEachPage = new ArrayList<>();
+		int currentCountOfTasks = 0;
+		int currentHeight = 0;
+		for (int i = 0; i < numberOfTasks; i++) {
+			if (currentHeight + heights[i] > 450) {
+				numberOfTasksOnEachPage.add(currentCountOfTasks);
+				currentCountOfTasks = 1;
+				currentHeight = heights[i];
+			} else {
+				currentCountOfTasks++;
+				currentHeight += heights[i];
+			}
+		}
+		numberOfTasksOnEachPage.add(currentCountOfTasks);
+		
+	}
+
+	private int determineTaskHeight(Task task) {
+		if (!task.isFloatingTask()) {
+			return taskCompositeHeight;
+		} else {
+			int numberOfSlots = task.getPossibleTime().size();
+			boolean hasTags = task.getTags().size() > 0;
+			int numberOfLines = numberOfSlots;
+			if (hasTags) {
+				numberOfLines++;
+			}
+			if (numberOfLines == 3) {
+				return taskCompositeHeightForThreeLines;
+			} else {
+				return (numberOfLines - 3) * taskCompositeIncrement
+						+ taskCompositeHeightForThreeLines;
+			}
+		}
+	}
+
+	private String determineTitle() {
+		switch (displayMode) {
+		case TODAY:
+			return Constants.MODE_TODAY;
+		case TOMORROW:
+			return Constants.MODE_TOMORROW;
+		case DEADLINE:
+			return Constants.MODE_DEADLINE;
+		case FLOATING:
+			return Constants.MODE_FLOATING;
+		case TIMED:
+			return Constants.MODE_TIMED;
+		case UNTIMED:
+			return Constants.MODE_UNTIMED;
+		case SEARCH:
+			return Constants.MODE_SEARCH;
+		case OVERDUE:
+			return Constants.MODE_OVERDUE;
+		case ALL:
+			return Constants.MODE_ALL;
+		case TODO:
+			return Constants.MODE_TODO;
+		case DATE:
+			return Constants.dateOnlyFormat.print(currentDisplayDateTime);
+		default:
+			return "Congrats! You have managed to break our application!";
+		}
+	}
+	
+	protected void displayTasks() {
+		if (pageNumber > numberOfTasksOnEachPage.size()) {
+			pageNumber = numberOfTasksOnEachPage.size();
+		}
+		if (pageNumber <= 0) {
+			pageNumber = 1;
+		}
+
+		int startingIndex = 0;
+		for (int i = 0; i < pageNumber - 1; i++) {
+			startingIndex += numberOfTasksOnEachPage.get(i);
+		}
+		
+		ArrayList<Task> taskList = logic.getTasksToDisplay(displayMode, currentDisplayDateTime);
+
+		taskComposites = new Composite[numberOfTasksOnEachPage.get(pageNumber - 1)];
+
+		for (int i = 0; i < taskComposites.length; i++) {
+			taskComposites[i] = new TaskComposite(displayTask,
+					taskList.get(startingIndex + i), startingIndex + i + 1);
+		}
+		
+		displayTask.pack();
+	}
+
+	protected void processFeedbackObject(Feedback feedbackObj,
+			HelpDialog helpDialog) {
+		switch (feedbackObj.getCommand()) {
+		case ADD:
+			this.setDisplayMode(DisplayMode.TODO);
+			this.setPageNumber(Integer.MAX_VALUE);
+			displayStateHistory.addDisplayState(DisplayMode.ALL,
+					Integer.MAX_VALUE);
+			break;
+		case EDIT:
+		case DELETE:
+		case DONE:
+		case FINALISE:
+			if (!feedbackObj.isErrorMessage()) {
+				this.setPageNumber(getPage(feedbackObj.getTaskIndex()));
+			}
+			displayStateHistory.addDisplayState(this.getDisplayMode(),
+					this.getPageNumber());
+			break;
+		case DISPLAY:
+			this.setDisplayMode(feedbackObj.getDisplayMode());
+			if (this.getDisplayMode() == DisplayMode.DATE) {
+				this.setDisplayDateTime(feedbackObj.getDisplayDate());
+			}
+			this.setPageNumber(Constants.DEFAULT_PAGE_NUMBER);
+			displayStateHistory.addDisplayState(this.getDisplayMode(),
+					this.getPageNumber());
+			break;
+		case SEARCH:
+			this.setPageNumber(Constants.DEFAULT_PAGE_NUMBER);
+			this.setDisplayMode(DisplayMode.SEARCH);
+			break;
+		case GOTO:
+			this.setPageNumber(feedbackObj.getGotoPage());
+			break;
+		case SORT:
+		case CLEAR:
+			this.setPageNumber(Constants.DEFAULT_PAGE_NUMBER);
+			this.setDisplayMode(DisplayMode.TODO);
+			displayStateHistory.addDisplayState(this.getDisplayMode(),
+					this.getPageNumber());
+			break;
+		case UNDO:
+			this.setDisplayMode(displayStateHistory.getCurrentDisplayMode());
+			this.setPageNumber(displayStateHistory.getCurrentPageNumber());
+			displayStateHistory.undo();
+			break;
+		case REDO:
+			displayStateHistory.redo();
+			this.setDisplayMode(displayStateHistory.getCurrentDisplayMode());
+			this.setPageNumber(displayStateHistory.getCurrentPageNumber());
+			break;
+		case HELP:
+			helpDialog.open(feedbackObj);
+		case EXIT:
+		case INVALID:
+		default:
+		}
+	}
+
+}
