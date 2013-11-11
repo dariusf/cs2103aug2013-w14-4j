94b4e37379e10a161d77e6cb55abd40066a46907
Chun Mei <Chun Mei@localhost>
2013-11-11 08:28:52 +0800

    Cleaned up ApplicationWindow and refactored

---
+++ src/parser/StateDefault.java
@@ -0,0 +1,60 @@
+package parser;
+
+import common.Constants;
+import common.Interval;
+
+//@author A0097282W
+class StateDefault implements State {
+
+	StringBuilder words = new StringBuilder();
+	private final Parser parser;
+	
+	Interval onAtUntilInterval = new Interval();
+	
+	public StateDefault(Parser parser) {
+		this.parser = parser;
+	}
+	
+	@Override
+	public void processToken(Token t) {
+		if (t instanceof TimeToken || t instanceof DateToken) {
+			parser.parseStates.push(new StateInterval(parser, this));
+			// Do not advance
+		}
+		else if (t instanceof WordToken) {
+			if (t.contents.equals(Constants.PARSER_KEYWORD_BY) || t.contents.equals(Constants.PARSER_KEYWORD_BEFORE)) {
+				parser.parseStates.push(new StateDeadline(parser, this, t.contents));
+			}
+			else {
+				words.append(t.contents + " ");
+			}
+			parser.tokens.nextToken();
+		}
+		else if (t instanceof TagToken) {
+			parser.tags.add(((TagToken) t).contents);
+			parser.tokens.nextToken();
+		}
+		else {
+			words.append(t.contents + " ");
+			parser.tokens.nextToken();
+		}
+	}
+
+	@Override
+	public boolean popCondition() {
+		// This is the starting state, so it never pops until the very end
+		return false;
+	}
+
+	@Override
+	public void onPop() {
+		parser.description = words.toString().trim();
+		if (onAtUntilInterval.isSet()) {
+			parser.intervals.add(onAtUntilInterval);
+		}
+	}
+
+	@Override
+	public void onPush() {
+	}
+}
